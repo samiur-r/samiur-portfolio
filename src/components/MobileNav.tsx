8f330fd15b00ff580db655676718f228de74bfee
“samiur-r” <samiurrahman.dev@gmail.com>
2023-06-27 00:20:58 +0600

    chore: test mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, Fragment, SetStateAction } from 'react'
+import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
