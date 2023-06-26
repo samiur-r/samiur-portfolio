51d78c14f0dda93cd1b5cfb1e2ae4abc74541445
“samiur-r” <samiurrahman.dev@gmail.com>
2023-06-26 22:08:59 +0600

    chore: test mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,8 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { Bars3Icon } from '@heroicons/react/24/solid'
-// import MobileNav from './MobileNav'
-import MobileNav2 from './MobileNav2'
+import MobileNav from './MobileNav2'
 
 const Nav = () => {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -26,8 +25,7 @@ const Nav = () => {
       id="header"
       className={`${isScrolled ? 'bg-[#0f3443]' : ''} sticky w-full z-30 top-0`}
     >
-      {/* {isOpen && <MobileNav open={isOpen} handleSetOpen={setIsOpen} />} */}
-      <MobileNav2 open={isOpen} handleSetOpen={setIsOpen} />
+      <MobileNav open={isOpen} handleSetOpen={setIsOpen} />
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="pl-4 flex items-center">
           <Link href="/">
