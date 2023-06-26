4341dc4d88e1818345f5a17e7affb751426e2344
“samiur-r” <samiurrahman.dev@gmail.com>
2023-06-26 21:32:18 +0600

    test: mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -41,12 +41,12 @@ const Nav = () => {
           </Link>
         </div>
         <div className="block lg:hidden pr-4">
-          <button className="flex items-center p-1 text-[#34e89e] hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+          <div className="flex items-center p-1 text-[#34e89e] hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
             <Bars3Icon
               className="w-9 h-9 text-[#34e89e]"
               onClick={() => setIsOpen(!isOpen)}
             />
-          </button>
+          </div>
         </div>
         <div
           className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
