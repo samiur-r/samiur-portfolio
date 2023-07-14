b6f4067ab4503495bd4ab3f74270ee7aebe118a1
“samiur-r” <samiurrahman.dev@gmail.com>
2023-07-15 00:57:57 +0600

    feat: add project details page

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -4,9 +4,11 @@ interface Project {
   id: number
   slug: string
   title: string
-  href: string
+  href?: string
   thumbnail: string
   tech: string
+  description: string
+  images?: Array<{ url: string; width: number; height: number }>
 }
 
 const projects: Project[] = [
@@ -14,73 +16,115 @@ const projects: Project[] = [
     id: 1,
     slug: 'subscription-based-e-commerce',
     title: 'Subscription based e-commerce',
-    href: '/project/different-dog',
+    href: 'https://differentdog.com/',
     thumbnail: '/projects/different-dog/1.png',
-    tech: 'React, TailwindCSS, BuilderIO, Node, AWS'
+    tech: 'React, TailwindCSS, BuilderIO, Node, AWS',
+    description: `**Frontend with React.ts, Tailwind CSS, and Efficient State Management with Redux**\n\nBuilt with React.ts and Tailwind CSS, the frontend of this platform offers a visually stunning and user-friendly interface. With Redux for efficient state management, enjoy seamless data flow and enhanced performance, delivering a flawless user experience.\n\n**Robust Backend Powered by Node.js with Secure and Scalable AWS Infrastructure**\n\nExperience a robust backend powered by Node.js, ensuring high performance, scalability, and reliability. Coupled with the secure and scalable infrastructure of AWS, this platform offers seamless hosting, storage, and cloud services. Leverage the power of AWS to effortlessly handle the growing demands of your e-commerce business.\n\n**Effortless Content Management with Builder.io**\n\nBuilder.io empowers to effortlessly manage platform's content. Update and modify content with ease, keeping subscribers engaged with fresh and exciting offerings. Streamline your content management process for enhanced efficiency.`,
+    images: [
+      { url: '/projects/different-dog/2.png', width: 1920, height: 9111 },
+      { url: '/projects/different-dog/3.png', width: 1920, height: 3856 }
+    ]
   },
   {
     id: 2,
-    slug: 'real-state',
-    title: 'Real State',
-    href: '/project/boshamlan',
+    slug: 'boshamlan',
+    title: 'Boshamlan (Real State)',
+    href: 'https://kw.boshamlan.com',
     thumbnail: '/projects/boshamlan/1.png',
-    tech: 'Next, TailwindCSS, Node, PostgreSQL'
+    tech: 'Next, TailwindCSS, Node, PostgreSQL',
+    description: `The Real Estate project is a dynamic and modern web application that leverages advanced technologies to deliver a seamless experience for users in the real estate industry. Built using **Next.ts**, **TailwindCSS**, **Zustand**, **Node.ts**, **PostgreSQL**, and a robust **payment gateway**, this project showcases cutting-edge development practices and offers a range of features to enhance the overall user experience.\n\nOne of the key highlights of this project is the comprehensive **admin panel**, which empowers administrators to efficiently manage the platform's various functionalities. From overseeing **user accounts** and **company profiles** to monitoring **subscriptions** and generating **reports**, the admin panel provides a centralized hub for streamlined operations.\n\nTo ensure secure access and **transactions**, the project incorporates a reliable **OTP** (One-Time Password) authentication system. This feature enhances the overall security of the platform and protects user data, offering peace of mind to both buyers and sellers.\n\nThe user-centric design extends to the inclusion of user accounts and company accounts. These personalized profiles allow individuals and organizations to showcase their listings, manage property information, and interact with potential buyers or lessees effectively.`,
+    images: [
+      { url: '/projects/boshamlan/1.png', width: 1366, height: 768 },
+      { url: '/projects/boshamlan/2.png', width: 1080, height: 1080 }
+    ]
   },
   {
     id: 3,
-    slug: 'saas',
-    title: 'SAAS',
-    href: '/project/devcurate',
-    thumbnail: '/projects/devcurate/1.png',
-    tech: 'Vue, Bootstrap'
+    slug: 'venueQ',
+    title: 'VenueQ',
+    thumbnail: '/projects/venueq/1.png',
+    tech: 'Next, TailwindCSS, Node, MongoDB',
+    description: `This dynamic web application built using **Next.js**, **Node.js**, and **MongoDB**, revolutionizes the way people search, rent, and book venues. Inspired by the Airbnb concept, my app provides a seamless experience for users who are looking to find and reserve unique venues for various occasions. With a user-friendly interface, visitors can easily search for venues based on their preferences, such as location, size, amenities, and availability.\n\nAspiring venue owners also benefit from the platform, as they can easily create and manage listings for their spaces. Through a secure registration process, owners can showcase their venues with detailed descriptions, high-quality images, and pricing information. This opens up opportunities for them to reach a wider audience and generate additional income by connecting with interested renters.`,
+    images: [
+      { url: '/projects/venueq/1.png', width: 1702, height: 4000 },
+      { url: '/projects/venueq/2.png', width: 1298, height: 1478 },
+      { url: '/projects/venueq/3.png', width: 239, height: 4000 },
+      { url: '/projects/venueq/4.png', width: 1366, height: 767 }
+    ]
   },
   {
     id: 4,
     slug: 'product',
     title: 'Product',
-    href: '/project/plume',
+    href: 'https://www.plume.com/',
     thumbnail: '/projects/plume/1.png',
-    tech: 'Next, Styled Component, NetlifyCMS'
+    tech: 'Next, Styled Component, NetlifyCMS',
+    description: `This project utilizes **Next.ts**, **Netlify CMS**, and **Styled Components** to create a user-friendly website. It empowers owners to effortlessly update and personalize their online presence, allowing them to manage content with ease while ensuring optimal performance and a visually appealing design.`,
+    images: [
+      { url: '/projects/plume/1.png', width: 1366, height: 767 },
+      { url: '/projects/plume/2.png', width: 1106, height: 2348 },
+      { url: '/projects/plume/3.png', width: 615, height: 4000 }
+    ]
   },
   {
     id: 5,
     slug: 'blockchain-nlp',
     title: 'Blockchain & NLP',
-    href: '/project/ethfakenews',
     thumbnail: '/projects/ethfakenews/1.png',
-    tech: 'React, Solidity, Web3'
+    tech: 'React, Solidity, Web3',
+    description: `The **Blockchain** based Reputation System for Detecting and Classifying Fake News project integrated **NLP** and blockchain technologies. Using **React** and **Solidity**, we developed a user-friendly interface. NLP analyzed news articles for fake news detection, while blockchain served as a decentralized verification system. The project showcased the power of combining human and machine intelligence to combat misinformation.`,
+    images: [
+      { url: '/projects/ethfakenews/1.png', width: 1366, height: 767 },
+      { url: '/projects/ethfakenews/2.png', width: 1920, height: 1080 },
+      { url: '/projects/ethfakenews/3.png', width: 1366, height: 768 }
+    ]
   },
   {
     id: 6,
-    slug: 'venueQ',
-    title: 'VenueQ',
-    href: '/project/venueq',
-    thumbnail: '/projects/venueq/1.png',
-    tech: 'Next, TailwindCSS, Node, MongoDB'
+    slug: 'saas',
+    title: 'SAAS',
+    href: 'https://www.devcurate.co/',
+    thumbnail: '/projects/devcurate/1.png',
+    tech: 'Vue, Bootstrap',
+    description: `Devcurate is a **Vue.ts** based **SaaS** project that tackles interview scheduling challenges between hiring managers and candidates, especially in the hybrid working environment. By leveraging features like calendar integration and candidate availability tracking, Devcurate streamlines the process, saving time and effort. Experience the power of Devcurate and revolutionize your interview coordination today.`,
+    images: [{ url: '/projects/devcurate/1.png', width: 1366, height: 767 }]
   },
   {
     id: 7,
     slug: 'lms',
     title: 'LMS',
-    href: '/project/lms',
     thumbnail: '/projects/lms/1.png',
-    tech: 'Next, Material UI, Node, AWS, MySQL'
+    tech: 'Next, Material UI, Node, AWS, MySQL',
+    description: `A powerful Learning Management System (LMS) created using Next.js, Node.js and AWS. This innovative platform revolutionizes the way educators manage courses and enables users to effortlessly enroll in their desired learning experiences. With seamless course creation, secure payment processing, and personalized learning features, this LMS empowers educators and learners alike.`,
+    images: [
+      { url: '/projects/lms/1.png', width: 1344, height: 2022 },
+      { url: '/projects/lms/2.png', width: 375, height: 3525 },
+      { url: '/projects/lms/3.png', width: 1920, height: 1080 },
+      { url: '/projects/lms/4.png', width: 1990, height: 2056 }
+    ]
   },
   {
     id: 8,
     slug: 'web3',
     title: 'Web3',
-    href: '/project/babyapesrescue',
+    href: 'https://apevolutionclub.com/',
     thumbnail: '/projects/babyapesrescue/1.png',
-    tech: 'Next, Material UI, Web3'
+    tech: 'Next, Material UI, Web3',
+    description: `In this project that combines the cutting-edge technologies of Next.js and Web3. By harnessing the potential of blockchain technology and integrating it seamlessly with Next.js, I have developed a groundbreaking website that brings decentralized functionality and enhanced user experiences to the forefront.`,
+    images: [
+      { url: '/projects/babyapesrescue/1.png', width: 1366, height: 768 },
+      { url: '/projects/babyapesrescue/2.png', width: 1080, height: 1080 },
+      { url: '/projects/babyapesrescue/3.png', width: 1106, height: 5070 }
+    ]
   },
   {
     id: 9,
     slug: 'book-api',
     title: 'Book Api',
-    href: '/project/booklist',
     thumbnail: '/projects/booklist/1.png',
-    tech: 'Laravel'
+    tech: 'Laravel',
+    description: `The Book List Website is a **Laravel** based application with an admin panel that allows users to manage books using the **Google Books API**. It provides a user-friendly interface for browsing, searching, and discovering a wide range of books. The admin panel enables efficient book management, including adding, editing, and deleting books from the database. With advanced search functionality, users can easily find books by title, author, or genre.\n\nThis project leverages Laravel's robust framework and integrates with the Google Books API to offer a seamless experience for book enthusiasts. It combines an intuitive user interface, efficient book management, and access to an extensive library of books. Whether you're looking to organize your own collection or explore new reading materials, the Book List Website is the perfect platform for managing and discovering books.`,
+    images: [{ url: '/projects/booklist/1.png', width: 1802, height: 2048 }]
   }
 ]
 
--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -1,14 +1,81 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
+import Head from 'next/head'
 import { projects, Project } from 'content/projects'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useEffect } from 'react'
 
 interface ProjectPageProps {
   project: Project
 }
 
 const ProjectPage: React.FC<ProjectPageProps> = ({ project }) => {
+  const content = project.description.split('\n\n').map((paragraph) => (
+    <p
+      key={Math.random()}
+      className="my-3"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: paragraph.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+      }}
+    />
+  ))
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'instant'
+    })
+  }, [])
+
   return (
     <div>
-      <h1>{project.title}</h1>
+      <Head>
+        <title>Samiur</title>
+        <meta name="description" content="Samiur" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="sm:w-1/2 text-white min-h-screen sm:fixed gradient flex justify-center items-center">
+        <div className="w-max flex justify-center items-start flex-col gap-5 p-5 shadow-xl">
+          <h1 className="font-bold text-2xl text-center">{project.title}</h1>
+          <p>
+            <span className="font-semibold">Techs:</span> {project.tech}
+          </p>
+          <div className="sm:max-w-xl">{content}</div>
+          <div className="flex gap-3">
+            {project.href && (
+              <a
+                href={project.href}
+                target="_blank"
+                rel="noreferrer"
+                className="rounded-full bg-[#0f3443] py-4 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Live
+              </a>
+            )}
+            <Link href="/#projects">
+              <a className="rounded-full py-4 px-8 bg-white text-gray-900 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                Back
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="sm:w-1/2 relative sm:left-1/2 p-5 flex flex-col gap-20 py-20">
+        {project.images &&
+          project.images.length > 0 &&
+          project.images.map((image) => (
+            <Image
+              key={Math.random()}
+              src={image.url}
+              width={image.width}
+              height={image.height}
+              objectFit="contain"
+            />
+          ))}
+      </div>
     </div>
   )
 }
